4c7230a5964dcf2da07aef3ede02096fd1332990
Denchik <daniil_ramanov@mail.ru>
2025-05-07 11:34:20 +0400

    ConnectionString fix

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -4,7 +4,7 @@ using MvcMovie.Data;
 using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MvcMovieContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PagesMovieContext") ?? throw new InvalidOperationException("Connection string 'PagesMovieContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
